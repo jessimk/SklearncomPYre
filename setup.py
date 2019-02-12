f0daad2fb5515bf819d1f96beda495d6c103e48c
Birinder <samdsilva1469@gmail.com>
2019-02-12 13:04:41 -0800

    Edited the Directory structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='SklearncomPYre',
+    version='v0.0',
+    packages=['sklearncomPYre'],
+	url = ['https://github.com/UBC-MDS/SklearncomPYre'],
+    license='MIT', 
+	long_description=open('README.md').read(),
+	install_requires=['numpy','sklearn','pandas','matplotlib.pyplot']	
+)
