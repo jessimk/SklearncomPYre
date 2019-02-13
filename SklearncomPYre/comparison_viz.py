286929f6bd966d9b71b7db81159e16125664c6ba
Talha Siddiqui <talhaadnan100@gmail.com>
2019-02-12 18:17:44 -0800

    Tests added

--- SklearncomPYre/comparison_viz.py
+++ SklearncomPYre/comparison_viz.py
@@ -6,44 +6,75 @@ import matplotlib.pyplot as plt
 
 def comparison_viz(comparison, choice):
     '''
-      
-      The purpose of the function is to help User visualize the comparison of accuracies or time given in the comparison 
-      dataframe. It takes in a dataframe with 7 attributes i.e. model name, training & test scores, model variance, 
-      and the time it takes to fit, predict model and total time taken for both fit and predict. 
-      
-      The function outputs(saves in the root directory) a beautiful <a href="https://matplotlib.org">matplotlib</a> 
-      bar chart comparison of different models' training and test scores or the time it takes to fit and predict. 
-        
-      Parameters :
-      
-      Inputs:   
+    The purpose of the function is to help User visualize the comparison of accuracies or time given in the comparison
+    dataframe. It takes in a dataframe with 7 attributes i.e. model name, training & test scores, model variance,
+    and the time it takes to fit, predict model and total time taken for both fit and predict.
 
-      comparison : Dataframe with 7 columns
-      - regressor or classifier name 
-      - training accuracy 
-      - test accuracy 
-      - model variance 
-      - time it takes to fit 
-      - time it takes to predict and 
-      - total time. Type: `pandas.Dataframe
-      - Choice of `accuracy` or `time`. Type: `string`
+    The function outputs(saves in the root directory) a beautiful <a href="https://matplotlib.org">matplotlib</a>
+    bar chart comparison of different models' training and test scores or the time it takes to fit and predict.
 
-      Outputs:
+    Parameters :
 
-      - Bar chart of accuracies or time comparison by models. Type: `png` 
-    
+    Inputs:
+
+    comparison : Dataframe with 7 columns
+    - regressor or classifier name
+    - training accuracy
+    - test accuracy
+    - model variance
+    - time it takes to fit
+    - time it takes to predict and
+    - total time. Type: `pandas.Dataframe
+    - Choice of `accuracy` or `time`. Type: `string`
+
+    Outputs:
+    - Bar chart of accuracies or time comparison by models. Type: `png`
+
 	inspiration: https://matplotlib.org/gallery/statistics/barchart_demo.html
     '''
-    
-    n_models = df.shape[0]
+
+    ## Tests
+
+	# Choice value
+
+    if (choice != 'time') and (choice != 'accuracy'):
+        raise ValueError("Choice must either be 'time' or 'accuracy'")
+
+	# Choice Type
+
+    if type(choice) != str:
+        raise TypeError("Choice must be of type string")
+
+	# Comparison Value Rows
+
+    if comparison.shape[0] < 1:
+        raise ValueError("Comparison must at least have 1 row")
+
+	# Comparison dimensions
+
+    if len(comparison.shape)!= 2:
+        raise ValueError("Comparison must be 2 dimensional")
+
+	# Comparison Type
+
+    if type(comparison) != pandas.core.frame.DataFrame:
+        raise TypeError("Comparison must be of type pandas.core.frame.DataFrame")
+
+	# Comparison Value Columns
+
+    if (comparison.shape[1]!= 7) and (comparison.shape[1]!= 8):
+        raise ValueError("Comparison must contain 7 columns (excluding index)")
+
+	## Function
+    n_models = comparison.shape[0]
 
     if choice == 'accuracy':
-        x = df.iloc[:,2]
-        y = df.iloc[:,3]
+        x = comparison.iloc[:,2]
+        y = comparison.iloc[:,3]
         labels = ('Train Accuracy','Test Accuracy','Accuracy','Train and Test Accuracy by Model')
     elif choice == 'time':
-        x = df.iloc[:,5]
-        y = df.iloc[:,6]
+        x = comparison.iloc[:,5]
+        y = comparison.iloc[:,6]
         labels = ('Fit Time','Predict Time','Time (s)','Fit and Predict Time by Model')
 
     fig, ax = plt.subplots()
@@ -60,7 +91,7 @@ def comparison_viz(comparison, choice):
                     alpha=opacity, color='r',
                     label=labels[1])
 
-    ax.set_xticklabels(df.iloc[:,1])
+    ax.set_xticklabels(comparison.iloc[:,1])
     ax.set_xlabel('Models')
     ax.set_ylabel(labels[2])
     ax.set_xticks(index + bar_width / 2)
@@ -73,7 +104,7 @@ def comparison_viz(comparison, choice):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("comparison", help="Output of train_test_acc_time()")
-parser.add_argument("choice", help="accuracy or time", type=str)
+parser.add_argument("choice", help="accuracy or time")
 args = parser.parse_args()
 
 comparison_viz(args.comparison, args.choice)
--- SklearncomPYre/test/test_comparison_viz.py
+++ SklearncomPYre/test/test_comparison_viz.py
@@ -11,11 +11,11 @@ import comparison_viz
 # Test Dataframe
 df = pd.DataFrame({'index': [1, 2, 3, 4],
                    'models': ["Random Forest","MLPClassifier", "Logistic Regression","GaussianNB"],
-                   'Train Accuracy': [90, 82, 73, 74], 
-                   'Test Accuracy': [90, 82, 73, 74], 
+                   'Train Accuracy': [90, 82, 73, 74],
+                   'Test Accuracy': [85, 78, 69, 70],
                    'Variance': [90, 82, 73, 74],
-                   'Predict Time (s)': [90, 82, 73, 74], 
-                   'Fit Time (s)': [90, 82, 73, 74], 
+                   'Fit Time (s)': [3, 2, 2.5, 1.2],
+                   'Predict Time (s)': [1, 1.4, 0.9, 2.2],
                    'Total Time (s)': [90, 82, 73, 74]
                   })
 
@@ -24,18 +24,18 @@ def test_input_choice_value():
     Testing for entering an invalid string as a choice.
     """
     try:
-        comparison_viz.comparison_viz(comparison=df,  choice='model')
+        comparison_viz(comparison=df, choice = 'model')
     except(ValueError):
         assert True
     else:
         assert False
-        
+
 def test_input_choice_type():
     """
     Testing for entering an invalid type for choice.
     """
     try:
-        comparison_viz.comparison_viz(comparison=df,  choice=3)
+        comparison_viz(comparison=df, choice=3)
     except(TypeError):
         assert True
     else:
@@ -47,18 +47,18 @@ def test_input_comparison_value():
     Testing for entering an invalid dataframe as a comparison.
     """
     try:
-        comparison_viz.comparison_viz(comparison=df['Train Accuracy'],  choice='time')
+        comparison_viz(comparison=df['Train Accuracy'], choice='time')
     except(ValueError):
         assert True
     else:
         assert False
-        
+
 def test_input_comparison_type():
     """
     Testing for entering an invalid type for choice.
     """
     try:
-        comparison_viz.comparison_viz(comparison=3,  choice='time')
+        comparison_viz(comparison=3, choice='time')
     except(TypeError):
         assert True
     else:
