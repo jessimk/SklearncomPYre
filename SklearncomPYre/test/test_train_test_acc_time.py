1a4ad561168807f219f975c90611da38974d3243
Birinder <samdsilva1469@gmail.com>
2019-02-12 20:02:45 -0800

    Final function and tests

--- SklearncomPYre/test/test_train_test_acc_time.py
+++ SklearncomPYre/test/test_train_test_acc_time.py
@@ -1,4 +1,3 @@
-
 # import packages
 import os
 import numpy as np
@@ -8,8 +7,7 @@ import sys
 
 # sklearn packages
 from sklearn import datasets
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
@@ -20,7 +18,11 @@ sys.path.insert(0, os.path.abspath("../../SklearncomPYre"))
 import SklearncomPYre
 
 # sample inputs
-dictionary = {'knn': KNeighborsClassifier(), 'LogRegression':LogisticRegression() , 'RForest': RandomForestClassifier()}
+dictionary = {
+    'knn': KNeighborsClassifier(),
+    'LogRegression':LogisticRegression() ,
+    'RForest': RandomForestClassifier()}
+
 
 iris = load_iris()
 X = iris.data[:, [2, 3]]
@@ -31,7 +33,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=123)
 # test train_test_acc_time(dictionary, X_train, y_train, X_test, y_test)
 
 # input type test for the Dictionary and the Input Arrays (X_train, y_train, X_test, y_test)
-
 def test_dict_type():
     with pytest.raises(TypeError):
         SklearncomPYre.train_test_acc_time('birinder', X_train, y_train, X_test, y_test),
@@ -48,4 +49,16 @@ def test_X_array_type():
 
 def test_inputs_given():
     with pytest.raises(TypeError):
-        SklearncomPYre.train_test_acc_time({},  y_train, X_test, y_test)
+        SklearncomPYre.train_test_acc_time(dictionary,  y_train, X_test, y_test)
+
+# Output type test for the function. The output should be a DataFrame
+def test_output_type():
+    result_dataframe = SklearncomPYre.train_test_acc_time(dictionary, X_train, y_train, X_test, y_test)
+    assert(type(result_dataframe) != int), "The type of output  is not DataFrame"
+    assert(type(result_dataframe) != list), "The type of output is not DataFrame"
+
+# output columns number check , The output should have 7 columns corresponding to accuracies and time taken.
+def test_output_dimension():
+    results_dataframe = SklearncomPYre.train_test_acc_time(dictionary, X_train, y_train, X_test, y_test)
+    print(results_dataframe.shape)
+    assert(results_dataframe.shape[1] == 7), "The number of columns in output DataFrame should be 7 "
--- SklearncomPYre/train_test_acc_time.py
+++ SklearncomPYre/train_test_acc_time.py
@@ -4,44 +4,92 @@
 # In[ ]:
 
 
+import pandas as pd
+import numpy as np
+import time
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC, SVR
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 def train_test_acc_time(models,X_train,y_train,X_test,y_test):
-    
-	
-	
+
     """
-    
-    The purpose of this function is to compare different sklearn regressors or classifiers in terms 
-    of training and test accuracies, and the time it takes to fit and predict. 
-    The function inputs are dictionary of models, input train samples `Xtrain`(input features), 
-    input test samples `Xtest`, target train values `ytrain` and target test values `ytest` 
+
+    The purpose of this function is to compare different sklearn regressors or classifiers in terms
+    of training and test accuracies, and the time it takes to fit and predict.
+    The function inputs are dictionary of models, input train samples `Xtrain`(input features),
+    input test samples `Xtest`, target train values `ytrain` and target test values `ytest`
     (continuous or categorical).
-    
-    The function outputs a beautiful dataframe with training & test scores, 
-    model variance, and the time it takes to fit and predict using different models.  
 
-  Inputs:
-
+    The function outputs a beautiful dataframe with training & test scores,
+    model variance, and the time it takes to fit and predict using different models.
+    Inputs:
     - Dictionary of ML classifiers or regressors.
     - X train set: `Array-like `
     - Y train set: `Array-like`
     - X test set: `Array-like `
     - Y test set: `Array-like`
+    Outputs:
+    Dataframe with 7 columns:
+    - regressor or classifier name
+    - training accuracy
+    - test accuracy
+    - model variance
+    - time it takes to fit
+    - time it takes to predict
+    - total time
 
-  Outputs:
+    The dataframe will be sorted by test score in descending order.
 
-    Dataframe with 7 columns: 
-    - regressor or classifier name 
-    - training accuracy 
-    - test accuracy 
-    - model variance 
-    - time it takes to fit 
-    - time it takes to predict 
-    - total time 
- 
-     The dataframe will be sorted by test score in descending order.
- 
-     """
-    
-    
-    pass
+    """
+    #initializing an empty results dictionary, an empty list and an empty dataframe
+    results_dict = {'Classifier':[],
+        'Train Accuracy':[],
+        'Test Accuracy':[],
+        'Fit Time':[],
+        'Predict Time':[]}
 
+
+    results_row =[]
+    results_df = pd.DataFrame()
+
+    if  type(models) != dict:
+        raise TypeError("Models aren't the right type. Try again ¯\_(ツ)_/¯ ")
+
+    inputs = [X_train, y_train, X_test, y_test]
+
+    for dataset in inputs:
+        if  type(dataset) != pd.DataFrame and type(dataset) != np.ndarray:
+            raise TypeError("Input datasets aren't the right type. Try again ¯\_(ツ)_/¯ ")
+
+    for model_name, model in models.items():
+    # Train models and populate the results_dict
+        t = time.time()
+        model.fit(X_train, y_train)
+
+        results_dict['Classifier'] = model_name
+        results_dict['Fit Time'] = time.time() - t
+        results_dict['Train Accuracy'] = model.score(X_train, y_train)
+
+        t = time.time()
+        model.predict(X_test)
+        results_dict['Predict Time'] = time.time() - t
+
+        results_dict['Test Accuracy'] = model.score(X_test, y_test)
+
+        results_row.append(results_dict)
+        results_df = results_df.append(results_row)
+        results_row.pop()
+
+    results_df = results_df.reset_index()
+    results_df=results_df.drop(['index'],axis=1)
+
+    results_df["Variance"] = results_df["Train Accuracy"] - results_df["Test Accuracy"]
+    results_df["Total Time"] = results_df["Fit Time"] - results_df["Predict Time"]
+
+    results_df.sort_values(by='Test Accuracy', ascending=False)
+
+    return results_df
