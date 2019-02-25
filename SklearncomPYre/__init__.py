40dd3efdcf9e11ff56e7f077da3fc3be030db59c
Talha Siddiqui <talhaadnan100@gmail.com>
2019-02-24 16:12:58 -0800

    Minor upper-case edit to name

--- SklearncomPYre/__init__.py
+++ SklearncomPYre/__init__.py
@@ -1,6 +1,6 @@
 ## inititalization
-name = "sklearncomPYre"
+name = "SklearncomPYre"
 
-from sklearncomPYre.train_test_acc_time import train_test_acc_time
-from sklearncomPYre.split import split
-from sklearncomPYre.comparison_viz import comparison_viz
+from SklearncomPYre.train_test_acc_time import train_test_acc_time
+from SklearncomPYre.split import split
+from SklearncomPYre.comparison_viz import comparison_viz
